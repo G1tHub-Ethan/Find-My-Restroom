076a9dc910cd31ba4a134f28222fcfcbfa2a8c11
Ethan Yu <jethanyu@outlook.com>
2020-08-04 12:57:37 -0500

    finished setting up first view controller

---
+++ Find My Restroom/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  Find My Restroom
+//
+//  Created by Ethan Yu on 8/4/20.
+//  Copyright © 2020 Ethan Yu. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+
+}
