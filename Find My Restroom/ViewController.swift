610d65f6c4b6b5aaba0d8ffb69d1ff683e8d6f94
Ethan Yu <jethanyu@outlook.com>
2020-08-04 15:06:52 -0500

    finished setting up view controller layout and first view controller elements and sound effect.

--- Find My Restroom/ViewController.swift
+++ Find My Restroom/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer = AVAudioPlayer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let sound = Bundle.main.path(forResource: "flushing", ofType: "mp3")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!)) //helper function to call sound file
+        }
+        catch {
+            print(error)
+        }
     }
-
-
+    
+    @IBAction func goToNextViewOnTap(_ sender: Any) {
+        audioPlayer.play()  //plays sound
+        performSegue(withIdentifier: "FirstToSecondView", sender: self) //goes to next view controller
+    }
+    
 }
 
