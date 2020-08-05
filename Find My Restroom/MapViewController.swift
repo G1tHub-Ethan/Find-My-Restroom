1b8100b5ba36643f10b3269a72a76decac461168
Ethan Yu <jethanyu@outlook.com>
2020-08-04 22:02:49 -0500

    Added user location and zoom to current location

--- Find My Restroom/MapViewController.swift
+++ Find My Restroom/MapViewController.swift
@@ -7,11 +7,31 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var region = MKCoordinateRegion()
+    var mapItems = [MKMapItem]()
+    var selectedMapItem = MKMapItem()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        mapView.delegate = self
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    let location = locations.first!
+    let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+    let span = MKCoordinateSpan(latitudeDelta: 0.035, longitudeDelta: 0.035)
+    region = MKCoordinateRegion(center: center, span: span)
+    mapView.setRegion(region, animated: true)
     }
     
     
