cbb9a06fcc8435755f8e15b84477ba77f30577e6
Ethan Yu <jethanyu@outlook.com>
2020-08-05 00:04:27 -0500

    Added pins that now have pop up details

--- Find My Restroom/MapViewController.swift
+++ Find My Restroom/MapViewController.swift
@@ -14,6 +14,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
     var region = MKCoordinateRegion()
+    var region2 = MKCoordinateRegion()
+    var region3 = MKCoordinateRegion()
     var mapItems = [MKMapItem]()
     var selectedMapItem = MKMapItem()
     
@@ -27,14 +29,77 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    let location = locations.first!
-    let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
-    let span = MKCoordinateSpan(latitudeDelta: 0.035, longitudeDelta: 0.035)
-    region = MKCoordinateRegion(center: center, span: span)
-    mapView.setRegion(region, animated: true)
+        let location = locations.first!
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.04, longitudeDelta: 0.04)
+        region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "restroom"
+        request.region = region
+        let request2 = MKLocalSearch.Request()
+        request2.naturalLanguageQuery = "fast food"
+        request2.region = region2
+        let request3 = MKLocalSearch.Request()
+        request3.naturalLanguageQuery = "gas station"
+        request3.region = region3
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    self.mapView.addAnnotation(annotation)
+                    self.mapItems.append(mapItem)
+                }
+            }
+        }
+        let search2 = MKLocalSearch(request: request2)
+        search2.start { (response, error) in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    self.mapView.addAnnotation(annotation)
+                    self.mapItems.append(mapItem)
+                }
+            }
+        }
+        let search3 = MKLocalSearch(request: request3)
+        search3.start { (response, error) in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    self.mapView.addAnnotation(annotation)
+                    self.mapItems.append(mapItem)
+                }
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: "pin")
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pinView")
+            pinView?.canShowCallout = true
+            pinView?.rightCalloutAccessoryView = UIButton(type: .infoLight)
+        }
+        else {
+            pinView?.annotation = annotation
+        }
+        return pinView
     }
     
     
     
-
 }
